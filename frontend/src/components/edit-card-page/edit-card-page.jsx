8270ffc032b6a1a1ef60be6ac5b2f4ca323b13f0
Nikol Zhurbenko <kindarufy@yandex.ru>
2026-05-07 21:14:30 +0300

    Fix image upload text

--- frontend/src/components/edit-card-page/edit-card-page.jsx
+++ frontend/src/components/edit-card-page/edit-card-page.jsx
@@ -144,7 +144,7 @@ export const EditCardPage = ({ data, setData, extraClass = "" }) => {
             <p className="text text_type_medium-16 text_color_primary">
               {currentFileName
                 ? currentFileName
-                : "Загрузите фото в фотрмате JPG"}
+                : "Загрузите фото в формате JPG"}
             </p>
           </label>
         )}
